efa3d2339f928e213a8ce75c26184bba89c1f8aa
dajac9 <139359605+dajac9@users.noreply.github.com>
2024-07-03 16:59:37 +0200

    termination and newhire payload created

--- scenarios/NewHire.js
+++ scenarios/NewHire.js
@@ -9,30 +9,37 @@ const {
   workDetailsSchema,
   payrollInfoSchema
 } = require("./schemas/events/hireEvent.json");
+
+const {birthInfoSchema, phoneNumbersSchema, emailIdsSchema, namesSchema} = require("./schemas/personSchemas.json");
 const { readProperty } = require("../utils/tools");
 
 async function createNewHire(startDate, params) {
   //creates person related data
-  const person = await readProperty(PersonSchema);
+  const person = await readProperty(PersonSchema, {"$startDate": startDate});
   const { managerFlag, hireDate } = params;
-  base_payload.data.paom.persons.push(person);
-  base_payload.data.paom.totalPages = 1;
-  base_payload.data.paom.totalRecords = base_payload.data.paom.persons.length;
-  // creates employment related data
-  const personId = base_payload.data.paom.persons[0].personId;
+  const personId = person.personId;
   const bindings = {
     "$startDate": startDate,
     "$employeeId": personId,
     "$mangerFlag": managerFlag,
     "$hireDate": hireDate,
   };
-  const empStatus = await readProperty(empStatusSchema, startDate, bindings);
-  const primaryInfo = await readProperty(primaryInfoSchema, startDate, bindings);
-  const joiningInfo = await readProperty(joiningInfoSchema, startDate, bindings);
-  const jobInfo = await readProperty(jobInfoSchema, startDate, bindings);
-  const contractInfo = await readProperty(contractInfoSchema, startDate, bindings);
-  const workDetails = await readProperty(workDetailsSchema, startDate, bindings);
-  const payrollInfo = await readProperty(payrollInfoSchema, startDate, bindings);
+  base_payload.data.paom.persons.push(person);
+  base_payload.data.paom.totalPages = 1;
+  base_payload.data.paom.totalRecords = base_payload.data.paom.persons.length;
+  //creates person related data
+  base_payload.data.paom.persons[0].birthInfo = [await readProperty(birthInfoSchema, bindings)];
+  base_payload.data.paom.persons[0].phoneNumbers = [await readProperty(phoneNumbersSchema, bindings)];
+  base_payload.data.paom.persons[0].emailIds = [await readProperty(emailIdsSchema, bindings)];
+  base_payload.data.paom.persons[0].names = [await readProperty(namesSchema, bindings)];
+  // creates employment related data
+  const empStatus = await readProperty(empStatusSchema, bindings);
+  const primaryInfo = await readProperty(primaryInfoSchema, bindings);
+  const joiningInfo = await readProperty(joiningInfoSchema, bindings);
+  const jobInfo = await readProperty(jobInfoSchema, bindings);
+  const contractInfo = await readProperty(contractInfoSchema, bindings);
+  const workDetails = await readProperty(workDetailsSchema, bindings);
+  const payrollInfo = await readProperty(payrollInfoSchema, bindings);
   base_payload.data.paom.persons[0].empStatus = [empStatus];
   base_payload.data.paom.persons[0].primaryInfo = [primaryInfo]
   base_payload.data.paom.persons[0].joiningInfo = [joiningInfo]
@@ -40,7 +47,7 @@ async function createNewHire(startDate, params) {
   base_payload.data.paom.persons[0].contractInfo = [contractInfo]
   base_payload.data.paom.persons[0].workDetails = [workDetails]
   base_payload.data.paom.persons[0].payrollInfo = [payrollInfo]
-  return [base_payload, startDate];
+  return [base_payload];
 }
 
 module.exports = { createNewHire };
